91972ea5aff73e7b4880e528487880518189ae2b
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 22:23:05 +0100

    Combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of single-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i < 57)
+{
+putchar(44);
+}
+}
+putchar('\n');
+return (0);
+}
