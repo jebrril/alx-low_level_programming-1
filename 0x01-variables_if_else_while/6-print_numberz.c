3a040c2d39856d25ccdff9ec6faaf5ccc7995a56
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 14:15:41 +0100

    0-9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: print 0-9 with \n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar("\n");
+return (0);
+}
