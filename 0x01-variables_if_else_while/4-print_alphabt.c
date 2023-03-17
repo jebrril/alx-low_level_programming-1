9a04d0043d52564e3e48675bf36983f6a301a515
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 13:42:01 +0100

    a-z without e and q with \n

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: print alpha from a-z without e & q with \n
+ * Return: Always 0 (success)
+ */
+int main()
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+	if (i != 'e' && i != 'q')
+	{
+	    putchar(i);
+	}
+}
+putchar('\n');
+return 0;
+}
