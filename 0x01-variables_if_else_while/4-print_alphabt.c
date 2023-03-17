89e1a5bebaa2483a76d85c32ae096a6f6ef75178
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 14:05:48 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 int i;
 for (i = 97; i <= 122; i++)
 {
-	if (i != 'e' && i != 'q')
-	{
-	    putchar(i);
-	}
+if (i != 'e' && i != 'q')
+{
+	putchar(i);
+}
 }
 putchar('\n');
 return (0);
