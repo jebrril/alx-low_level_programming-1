6c9e7b1dd99dd1462f7f61071a5b4ec47b8a3d45
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 13:06:46 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
+ */
+int main() {
+    int i;
+    for (i = 97; i <= 122; i++) {
+        putchar(i);
+    }
+    return 0;
+}
