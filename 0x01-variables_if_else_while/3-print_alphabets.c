a84a51111b474a55665a4610687d52752b06dac0
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-17 13:23:34 +0100

    a-z A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: print alpha from a-z and A-Z with \n
+ * Return: Always 0 (success)
+ */
+int main()
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+	putchar(i);
+}
+for (i = 65; i <= 90; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return 0;
+}
