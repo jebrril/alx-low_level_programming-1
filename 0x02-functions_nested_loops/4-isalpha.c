d37f1eafa12e0f3dedb6a9a510a7a009db0ff7ee
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-21 23:53:57 +0000

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isalpha -  shows 1 if the input is a letter otherwise 0.
+*
+* @c: The character in ASCII
+*
+* Return: 1 if a letter, otherwise 0.
+*/
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
