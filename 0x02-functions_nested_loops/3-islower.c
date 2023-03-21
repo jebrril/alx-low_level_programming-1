e51641e57397b96d94e485d603c7b02402a5c2d0
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-21 23:01:06 +0000

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _islower -  checks for lowercase character.
+*
+* Return : Always 0 (success)
+*/
+void print_alphabet_x10(void){
+if (c >= 'a' && c <= 'z') {
+return (1);
+} else {
+return (0);
+}
+}
