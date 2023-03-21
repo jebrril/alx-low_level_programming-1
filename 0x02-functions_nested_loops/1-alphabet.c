36aa9bbee05cc72d1c22159e88249baecbc68540
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-21 17:55:23 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 * main - Entry point
 * print_alphabet - shows the lowercase alphabet with \n.
-* function print_alphabet - mained
+* description function print_alphabet - mained
 */
 void print_alphabet(void)
 {
