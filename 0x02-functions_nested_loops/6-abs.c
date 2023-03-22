c4f4d79db14aa83a698a538d8649bf5ebfb72d3a
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-22 00:15:35 +0000

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _abs -  computes abs value of int.
+*
+* @i: the integer value input
+*
+* Return: absolute value
+*/
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
