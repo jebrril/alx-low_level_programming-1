79ad912d5519e7765667d393191433feb26cae12
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-03-22 00:04:54 +0000

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign -  Check if number negative or positive or 0.
+*
+* @: The integer input number
+*
+* Return: 1 if > 0 and 1 if < 0, otherwise 0.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43)
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45)
+return (-1);
+}
+else
+{
+_putchar(48)
+return (0);
+}
+_putchar('\n');
+}
