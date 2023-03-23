05003c778469fbb9e15b4157fef170567090f740
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-03-23 18:28:23 +0000

    first one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isupper - this function check if char is upper or not
+*
+* @c - variable to be checked
+*
+* Return: 1 if the char upper otherwise 0
+*/
+int _isupper(int c)
+{
+    if(c >= 65 && c <= 90)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project is for 0x04. C - More functions, more nested loops task in ALX school
