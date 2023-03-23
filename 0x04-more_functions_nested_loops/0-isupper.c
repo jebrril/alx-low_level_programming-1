8124cc9ba437c694aeecd6eff2ae2e58bc3e5fc0
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-03-23 18:31:10 +0000

    Updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _isupper - this function check if char is upper or not
-*
-* @c - variable to be checked
-*
-* Return: 1 if the char upper otherwise 0
-*/
+ * _isupper - this function check if char is upper or not
+ *
+ * @c - variable to be checked
+ *
+ * Return: 1 if the char upper otherwise 0
+ */
 int _isupper(int c)
 {
     if(c >= 65 && c <= 90)
